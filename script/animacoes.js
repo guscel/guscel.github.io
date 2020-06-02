c1fe5141421ad1f45de3774295287c5ee87e5931
Gustavo Mendel <gustavo.mende.br@gmail.com>
2020-06-02 14:39:47 -0300

    adicionado animações completas

--- script/animacoes.js
+++ script/animacoes.js
@@ -1,3 +1,5 @@
+// Funcão para navegação suave pelo menu navbar.
+
 $('nav a').click(function(event) {
     event.preventDefault();
 
@@ -9,26 +11,54 @@ $('nav a').click(function(event) {
     }, 500);
 });
 
-var $target = $('.anime');
-var $target_special_1 = $('.anime-special-1');
-var $target_special_2 = $('.anime-special-2');
-var offsetop = $(window).height() * 3/4;
+// Debounce do Lodash
 
-function animeScroll() {
-    var documentTop = $(document).scrollTop();
-    
-    $target.each(function() {
-        var itemTop = $(this).offset().top;
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
-        if (documentTop > itemTop) {
-            $(this).addClass('anime-start');
-        } else {
-            $(this).removeClass('anime-start');
-        }
-    });
 
-    $target_special_1.addClass('anime-start')
-    $target_special_2.addClass('anime-start')
-}
+// Funções para animações
 
-animeScroll();
+(function() {
+    var $target = $('.anime');
+    var $target_special_1 = $('.anime-special-1');
+    var $target_special_2 = $('.anime-special-2');
+    var offsetop = $(window).height() * 4/5;
+
+    function animeScroll() {
+        var documentTop = $(document).scrollTop();
+        
+        $target.each(function() {
+            var itemTop = $(this).offset().top;
+            console.log("entrou");
+
+            if (documentTop > itemTop - offsetop) {
+                $(this).addClass('anime-start');
+            } else {
+                $(this).removeClass('anime-start');
+            }
+        });
+
+        $target_special_1.addClass('anime-start')
+        $target_special_2.addClass('anime-start')
+    }
+
+    animeScroll();
+
+    $(document).scroll(debounce(function() {
+        animeScroll();
+    }, 50));
+
+}());
