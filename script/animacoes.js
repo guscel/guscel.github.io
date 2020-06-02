3d0ebe079c8f6e9a717b846a3c9ab251a1820118
Gustavo Mendel <gustavo.mende.br@gmail.com>
2020-06-02 11:15:31 -0300

    adicionado primeiras animações do título Guscel

--- script/animacoes.js
+++ script/animacoes.js
@@ -1,4 +1,34 @@
-$('nav ul li a').click(function(event) {
+$('nav a').click(function(event) {
     event.preventDefault();
-    console.log('sucesso');
+
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+
+    $('body').animate({
+        scrollTop: targetOffset - 20
+    }, 500);
 });
+
+var $target = $('.anime');
+var $target_special_1 = $('.anime-special-1');
+var $target_special_2 = $('.anime-special-2');
+var offsetop = $(window).height() * 3/4;
+
+function animeScroll() {
+    var documentTop = $(document).scrollTop();
+    
+    $target.each(function() {
+        var itemTop = $(this).offset().top;
+
+        if (documentTop > itemTop) {
+            $(this).addClass('anime-start');
+        } else {
+            $(this).removeClass('anime-start');
+        }
+    });
+
+    $target_special_1.addClass('anime-start')
+    $target_special_2.addClass('anime-start')
+}
+
+animeScroll();
