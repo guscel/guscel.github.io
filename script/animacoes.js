e8514277663c64aefd33254bccfd410afb6a8ffe
Gustavo Mendel <gustavo.mende.br@gmail.com>
2020-06-03 09:33:58 -0300

    scroll suave adicionado

--- script/animacoes.js
+++ script/animacoes.js
@@ -1,15 +1,44 @@
 // Funcão para navegação suave pelo menu navbar.
 
-$('nav a').click(function(event) {
-    event.preventDefault();
+var menuItens = document.querySelectorAll('.scrollsuave a');
 
-    var id = $(this).attr('href'),
-        targetOffset = $(id).offset().top;
+menuItens.forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: targetOffset - 20
-    })
+        const element = event.target;
+        const id = element.getAttribute('href');
+        const top = document.querySelector(id).offsetTop;
+
+        smoothScrollTo(0, top, 1500);
+    });
 });
+
+function smoothScrollTo(endX, endY, duration) {
+    const startX = window.scrollX || window.pageXOffset;
+    const startY = window.scrollY || window.pageYOffset;
+    const distanceX = endX - startX;
+    const distanceY = endY - startY;
+    const startTime = new Date().getTime();
+  
+    duration = typeof duration !== 'undefined' ? duration : 400;
+  
+    // Easing function
+    const easeInOutQuart = (time, from, distance, duration) => {
+      if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+      return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+    };
+  
+    const timer = setInterval(() => {
+      const time = new Date().getTime() - startTime;
+      const newX = easeInOutQuart(time, startX, distanceX, duration);
+      const newY = easeInOutQuart(time, startY, distanceY, duration);
+      if (time >= duration) {
+        clearInterval(timer);
+      }
+      window.scroll(newX, newY);
+    }, 1000 / 60); // 60 fps
+  };
 
 // Debounce do Lodash
 
@@ -41,7 +70,6 @@ debounce = function(func, wait, immediate) {
         
         $target.each(function() {
             var itemTop = $(this).offset().top;
-            console.log("entrou");
 
             if (documentTop > itemTop - offsetop) {
                 $(this).addClass('anime-start');
@@ -58,6 +86,6 @@ debounce = function(func, wait, immediate) {
 
     $(document).scroll(debounce(function() {
         animeScroll();
-    }, 25));
+    }, 10));
 
 }());
